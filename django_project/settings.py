464897736658bd21c23b71dbd0a5d9f077e1d4c6
Nikita Gharin <77896451+cvclon3@users.noreply.github.com>
2024-08-02 22:09:47 +0300

    add/setup Blog API

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,19 +37,47 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # REST
+    'rest_framework',
+    # CORS
+    'corsheaders',
     # Local
     'accounts.apps.AccountsConfig',
+    'posts.apps.PostsConfig',
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    # CORS
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',    # port commonly used for React
+    'http://localhost:8000',
+)
+
+
+# https://docs.djangoproject.com/en/5.0/ref/settings/#csrf-trusted-origins
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000',    # port commonly used for React
+]
+
 
 ROOT_URLCONF = 'django_project.urls'
 
